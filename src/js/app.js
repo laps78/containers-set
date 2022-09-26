09d54348bb1db2d958d7e95bbc90cf66b2f378cf
laps78 <9254384@mail.ru>
2022-09-26 16:52:44 +0300

    milestone commit: tests in teamMe=mbers.test.js are not ok!

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,20 @@
 // TODO: write your code here
-import sum from './basic';
+export default class Team {
+  constructor() {
+    this.members = new Set();
+  }
 
-console.log('worked');
+  add(member) {
+    if (this.members.has(member)) throw new Error('Персонаж уже существует в команде!');
+    this.members.add(member);
+  }
 
-console.log(sum([1, 2]));
+  addAll(...members) {
+    members.forEach((member) => this.add(member));
+  }
+
+  toArray() {
+    const membersArray = [];
+    this.members.forEach((member) => membersArray.push(member));
+  }
+}
