ea07bc460ab8e8cea517ce2c90f6377c9c40b862
laps78 <9254384@mail.ru>
2022-09-26 19:10:10 +0300

    milestone commit: deploying broken tests to repo

---
+++ src/js/__tests__/teamMembers.test.js
@@ -0,0 +1,29 @@
+import Bowman from '../bowman';
+import Swordsman from '../swordsman';
+import Daemon from '../daemon';
+import Team from '../app';
+
+// init new characters
+const bowmanFred = new Bowman('Fred');
+const swordsmanTom = new Swordsman('Tom');
+const daemonKarl = new Daemon('Karl');
+
+// Init new team
+const team = new Team;
+team.add(swordsmanTom);
+
+test('class Team method add() should throw an error trying to add a character that already in team', () => {
+  function testError() {
+    team.add(swordsmanTom);
+  }
+  expect(testError).toThrowError('Персонаж уже существует в команде!');
+});
+
+test('class Team method add() should add a team member', () => {
+  team.add(bowmanFred);
+  expect(team.has(bowmanFred)).toBe(true);
+});
+
+test('team.adAll() method should add multiple characters', () => {
+  expect(true).toBe(true);
+});
